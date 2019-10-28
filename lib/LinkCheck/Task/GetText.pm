8464b59d4f888e94f52b7d7f7b2d7d4746fad7cf
Marco Arthur <arthurpbs@gmail.com>
2019-10-28 15:55:58 -0300

    Add GetText task
    It is similiar to check_link but gets all <p> elements of an url
    and save results to the database.

---
+++ lib/LinkCheck/Task/GetText.pm
@@ -0,0 +1,26 @@
+package LinkCheck::Task::GetText;
+use Mojo::Base 'Mojolicious::Plugin';
+
+sub register {
+    my ( $self, $app ) = @_;
+    $app->minion->add_task( get_text => \&_get_text );
+}
+
+sub _get_text {
+    my ( $job, $url ) = @_;
+
+    my @results;
+    my $ua  = $job->app->ua;
+    my $res = $ua->get($url)->result;
+    push @results, [ $url, $res->code ];
+
+    # get and save text to database
+    my $text = $res->dom->find('p')->map('text')->join("\n")->trim->to_string;
+    my $row  = {
+        site => $url,
+        text => $text,
+    };
+    $job->app->db->insert( 'texts', $row );
+
+    $job->finish( \@results );
+}
